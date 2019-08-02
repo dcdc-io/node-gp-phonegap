3d718c443f6c9c91564192a3c595033a1643e43a
ben@dcdc.io <ben@dcdc.io>
2019-08-02 23:35:03 +0100

    updated node-gp

---
+++ scripts/copybundle.js
@@ -0,0 +1,10 @@
+module.exports = function (context) {
+    var fs = require('fs')
+    let copy = ['node_modules/node-gp/browser/bundle.debug.js']
+    let dest_path = 'www/js/'
+
+    for (let i = 0; i < copy.length; i++) {
+        let name = copy[i].split('/').pop()
+        fs.createReadStream(copy[i]).pipe(fs.createWriteStream(dest_path + '/' + name))
+    }
+}
