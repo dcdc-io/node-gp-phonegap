4c0839e0de799f92ab86789143da4678c517058c
ben@dcdc.io <ben@dcdc.io>
2019-08-05 14:08:56 +0100

    updated node-gp name

--- scripts/copybundle.js
+++ scripts/copybundle.js
@@ -1,6 +1,6 @@
 module.exports = function (context) {
     var fs = require('fs')
-    let copy = ['node_modules/node-gp/browser/bundle.debug.js']
+    let copy = ['node_modules/node-gp/browser/nodegp-bundle.debug.js']
     let dest_path = 'www/js/'
 
     for (let i = 0; i < copy.length; i++) {
